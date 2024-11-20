3a4db0d83737729da0d5c700f677a1327e66e44e
suraphon <suraphon.k@merudy.com>
2024-11-20 11:38:06 +0700

    init tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,14 @@ export default defineNuxtConfig({
   },
 
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  css: ['~/assets/css/main.css'],
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
